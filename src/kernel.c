36dde5e46df7deafe7502d9e0df79608e2d55732
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2024-04-05 11:03:46 +0200

    Add files via upload

--- src/kernel.c
+++ src/kernel.c
@@ -10,7 +10,5 @@ void kernelmain(void)
 {
     vga_init();
     vga_clear();
-    vga_print(" [ info ] Kernel loaded! \n");
-    vga_print(" [ info ] VGA initialized! \n");
-    vga_print(" [ info ] Memory initialized! \n");
+    vga_print(" Welcome to my OS!\n");
 }
