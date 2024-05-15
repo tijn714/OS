e5575a24ca1192c9117767bf6286e907369d3b9c
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2024-05-15 23:17:37 +0200

    Delete include directory

--- include/gdt.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GDT_H__
-#define __GDT_H__
-
-#include "types.h"
-
-extern void gdt_init();
-extern void gdt_set_gate(int32_t num, unsigned long base, unsigned long limit, uint8_t access, uint8_t gran);
-
-#endif /* __GDT_H__ */
--- include/idt.h
+++
@@ -1,9 +0,0 @@
-#ifndef __IDT_H__
-#define __IDT_H__
-
-#include "types.h"
-
-void idt_init();
-void idt_set_gate(uint8_t num, unsigned long base, uint16_t sel, uint8_t flags);
-
-#endif /* __IDT_H__ */
