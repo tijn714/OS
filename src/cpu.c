38657be5ce7cf5662800ce057ec1c912a2f61e88
Tijn Rodrigo <71492011+tijn714@users.noreply.github.com>
2024-07-30 22:00:03 +0200

    Delete src directory

--- src/cpu.c
+++
@@ -1,8 +0,0 @@
-#include "cpu.h"
-#include "io_ports.h"
-
-void __cpuid(uint32_t type, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx) {
-    __asm__ __volatile__("cpuid"
-                         : "=a"(*eax), "=b"(*ebx), "=c"(*ecx), "=d"(*edx)
-                         : "a"(type));
-}
